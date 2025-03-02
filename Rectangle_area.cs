d328d837c9aeb10f7b40df481884b73ba830f350
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-02 23:15:59 +0300

    Rectangle_area.cs

---
+++ Rectangle_area.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 45;
+            int width = 76;
+            int area = length * width;
+            Console.Write("The Area of the given rectangle is " + area);
+        }
+    }
+}
