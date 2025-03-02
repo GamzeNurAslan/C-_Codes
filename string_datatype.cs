2e3ddfa69d8224e283bab16ecf1412072e829c35
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-02 23:18:55 +0300

    string_datatype.cs

---
+++ string_datatype.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Code";
+            string b = "Chef";
+            Console.Write(a);
+            Console.Write(b);
+        }
+    }
+}
