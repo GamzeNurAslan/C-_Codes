20b8852e429602262652883e46858378e26c9e20
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-02 23:17:51 +0300

    Area_of ​_the_circle.cs

---
+++ Area_of ​_the_circle.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+            double radius = 8.9;
+            double area = pi * radius * radius;
+            Console.Write("The Area of the given Circle is " + area);
+        }
+    }
+}
