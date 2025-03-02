8aae5a83ebc6afe9865fbc690e42e50d7ccbb399
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-02 23:22:52 +0300

    Hello_World :)).cs

---
+++ Hello_World :)).cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string x = "Hello";
+            string y = "World";
+            
+            Console.Write(x + " " + y);
+        }
+    }
+}
