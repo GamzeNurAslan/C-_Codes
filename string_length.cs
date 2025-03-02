b8c9b1d35ab9762ae583ee09ab36a6e41d906a48
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-02 23:25:58 +0300

    string_length.cs

---
+++ string_length.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "NumeroTres";
+            int a = txt.Length;
+            Console.Write("The length of the word is: " + a);
+        }
+    }
+}
