788251ca9eed5797e42f90475e16b0a63c8a5401
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-02 23:27:15 +0300

    Outputting Characters from a String.cs

---
+++ Outputting Characters from a String.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Programming";
+            Console.WriteLine(word[2]);
+            Console.Write(word[4]);
+        }
+    }
+}
