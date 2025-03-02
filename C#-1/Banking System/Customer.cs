43f7613a214f804c82c79cf4acc0a250861f120e
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-02 20:22:59 +0300

    Customer.cs

---
+++ C#-1/Banking System/Customer.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem.classes
+{
+    class Customer
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public Account Account { get; set; }
+
+    }
+}
