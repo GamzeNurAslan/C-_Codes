0f994d4c8acbe76712b7321c50419134af0d359a
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-03 09:34:52 +0300

    user_while_loop.cs

---
+++ user_while_loop.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            while (a < 10)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+        }
+    }
+}
