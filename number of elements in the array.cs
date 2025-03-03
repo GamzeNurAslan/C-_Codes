7facd53e62494f27ea9daed4b1900054b60e9058
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-03 09:31:08 +0300

    number of elements in the array.cs

---
+++ number of elements in the array.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] myNumbers = {10, 20, 30, 40, 50, 60};
+             int a = myNumbers.Length;
+             
+             Console.Write(a);
+        }
+    }
+}
