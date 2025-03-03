6abb561bef36133b72f8e384d5831cad181209bb
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-03 09:25:47 +0300

    array_definition.cs

---
+++ array_definition.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = {1, 2, 3, 4, 5};
+            Console.Write("Done");
+        }
+    }
+}
