fda1360d66012715c02626bf3c999329ca808782
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-03 09:20:37 +0300

    if_usage.cs

---
+++ if_usage.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if(a > b)
+            {
+                Console.Write("Coding is Fun!");
+            }
+        }
+    }
+}
