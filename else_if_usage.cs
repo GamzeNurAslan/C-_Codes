9f322ae7c07975661b674e68c32c081196230ab0
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-03 09:22:32 +0300

    else_if_usage.cs

---
+++ else_if_usage.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = 45;
+            int b = 23;
+            if(r > b)
+            {
+                Console.WriteLine("Rob Scored higher marks than Bob.");
+            }
+            else if(r == b)
+            {
+                Console.WriteLine("Bob & Rob both scored the same");
+            }
