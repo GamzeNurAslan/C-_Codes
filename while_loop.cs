fe04d52bc43a9756d38e7388832d79aa0093b10e
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-03-03 09:33:02 +0300

    while_loop.cs

---
+++ while_loop.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            while(a < 7){
+                Console.WriteLine(a);
+                a++;
+            }
+        }
+    }
+}
