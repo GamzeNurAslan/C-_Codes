aae7166910316d3aa55dfb5226b1739b9c843089
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-04-16 21:23:06 +0300

    Kişiler-telefon-nesne.cs

---
+++ Console Applications.../Kişiler-telefon-nesne.cs
@@ -0,0 +1,44 @@
+class Kisiler
+{
+    string ad;
+    string soyad;
+    string telefon;
+
+    public string Ad
+    {
+        get
+        {
+            return ad;
+        }
+
+        set
+        {
+            ad = value;
+        }
+    }
+
+    public string Soyad
+    {
+        get
+        {
+            return soyad;
+        }
+
+        set
+        {
+            soyad = value;
+        }
+    }
+
+    public string Telefon
+    {
+        get
+        {
+            return telefon;
+        }
+
+        set
+        {
+            telefon = value;
+        }
+    }
