84c9ae4624a7663e12aab1aa30a86b8acf04760b
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-04-18 07:59:39 +0300

    Girilen Karakterin ASCII Karşılığı.cs

---
+++ Console Applications.../Girilen Karakterin ASCII Karşılığı.cs
@@ -0,0 +1,8 @@
+static void Main(string[] args)
+        {
+            char c;
+            Console.Write("Karakter Girin: ");
+            c = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(c+ " karakteri ASCII Kaşılığı >> " + Convert.ToInt32(c));
+            Console.ReadKey();
+        }
