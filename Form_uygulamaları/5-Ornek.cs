0b3ea73f0510df98944d1b84871936e29391e671
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-05-03 18:41:01 +0300

    5-Ornek.cs

---
+++ Form_uygulamaları/5-Ornek.cs
@@ -0,0 +1,22 @@
+using System.Windows.Forms;
+
+namespace WinFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DialogResult tus;
+            tus = colorDialog1.ShowDialog();
+            if (tus == DialogResult.OK)
+            {
+                this.BackColor = colorDialog1.Color;
+            }
+        }
+    }
+}
